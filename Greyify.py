4058089ac864119b99b248ae387ef23e68b52268
ghostlypi <parth.iyer@gmail.com>
2019-05-31 10:59:27 -0700

    Add files via upload

--- Greyify.py
+++ Greyify.py
@@ -1,8 +1,31 @@
-from PIL import Image
 import os
+from PIL import Image
 
-def grayscale(path):
+def splitPosNeg(path, seed, categories = ['pos', 'neg']):
+    os.mkdir(path + 'all')
+
+    filestring = ''
+    categoryCount = 0
+    dataCount = seed
+
+    for category in categories:
+        for filename in os.listdir(path + category + '/'):
+          os.rename(path + category + '/' + filename, path + 'all/' + str(dataCount) + '.' + filename.split('.')[1])
+          filestring += str(categoryCount) + ',';
+          dataCount += 1
+        categoryCount += 1
+        os.rmdir(path + category + '/')
+    
+    filestring = filestring[:-1]
+    with open(path + 'Lables.txt', 'w') as f:
+      f.write(filestring)
+
+# grayscale and set to 64x64 pixels
+def standardize(path):
     for filename in os.listdir(path):
-        img = Image.open(path + filename).convert('L')
-        os.remove(path + filename)
+        img = Image.open(path + filename).convert('L').resize( (64, 64) , Image.ANTIALIAS) # .convert('L') grayscales the image
         img.save(path + filename)
+
+def clean(path, seed):
+    splitPosNeg(path, seed)
+    standardize(path + "all/")
--- Video.py
+++ Video.py
@@ -1,20 +1,26 @@
 import numpy as np
 import cv2
 import Greyify as g
+import time
 
 cap = cv2.VideoCapture(0)
 
-count = 0
+def capture(path, limit):
+    count = int(np.random.random()*1000000)
+    limit = limit*10 + count
+    path += "\\"
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    while(count < limit):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    if count % 10 == 0:
-        cv2.imwrite("C:\\users\\parth\\Human Pictures\\Data\\Unsorted\\frame%d.jpg" % count, frame)
-    count += 1
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+        # Our operations on the frame come here
+        if count % 10 == 0:
+            cv2.imwrite( path+"frame%d.jpg" % count, frame)
+        count += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
